0ee23b5ce41dae805004954c921b946efd848942
Nikhil Jha <hi@nikhiljha.com>
2021-10-07 16:08:33 -0700

    :key: add secret extraction and conversion

--- ocfkube/utils/secrets.py
+++ ocfkube/utils/secrets.py
@@ -1,33 +1,54 @@
+from typing import Tuple
+import base64
+import os
 import hvac
 
 
-def get_path_from_secret(secret: dict):
-    pass
+def fix_base64(pairs: dict) -> dict:
+    """Runs base64^-1(v) on every value in a dict."""
+    {k: base64.b64decode(v) for k, v in pairs}
 
 
-def get_value_from_secret(secret: dict):
-    pass
+def extract_secret(secret: dict) -> Tuple[str, dict]:
+    """Takes a Kubernetes secret object, returns analogous Vault path and the k:v secrets."""
+    # TODO: Handle generateName? Though who would put generateName on a secret? Does that even work???
+    path = (
+        f"{secret['metadata'].get('namespace', 'default')}/{secret['metadata']['name']}"
+    )
+    return path, {**fix_base64(secret.get("data", {})), **secret.get("stringData", {})}
 
 
-def get_key_from_secret(secret: dict):
-    pass
-
-
-def process_secret(secret: dict, token: str, bootstrap: bool = False) -> dict:
+def convert_secret(secret: dict, bootstrap: bool = False) -> dict:
     """Takes a Kubernetes secret object, pushes it to Vault (if not bootstrap), returns VaultSecret object."""
     if bootstrap:
         return secret
-    path = get_path_from_secret(secret)
-    key = get_key_from_secret(secret)
+
     client = hvac.Client("https://vault.ocf.berkeley.edu")
-    client.token = token
-    assert client.is_authenticated()
-    client.secrets.kv.v2.configure(mount_point='pwned')
-    try:
-        data = client.secrets.kv.v2.read_secret_version(path=path, mount_point='pwned')
-        value = data["data"]["data"][key]
-    except hvac.exceptions.InvalidPath:
-        value = get_value_from_secret(secret)
-        client.secrets.kv.v2.create_or_update_secret(
-            path=path, secret={key: value}, cas=0, mount_point='pwned',
+    client.token = os.get("VAULT_TOKEN")
+    if not client.token:
+        print(
+            "I couldn't find your VAULT_TOKEN env variable, so I can't push secrets to Vault."
         )
+    assert client.is_authenticated()
+    client.secrets.kv.v2.configure(mount_point="pwned")
+
+    path, pairs = extract_secret(secret)
+    try:
+        client.secrets.kv.v2.create_or_update_secret(
+            path=path,
+            secret=pairs,
+            cas=0,
+            mount_point="pwned",
+        )
+    except hvac.exceptions.InvalidRequest:
+        # TODO: Try to get the secret, see if there are any new fields. If there are, add those to Vault.
+        # If there are extra fields, print an error so people looking at CI logs can see!
+        print(f"'{path}' already created, not re-creating")
+
+    return {
+        "apiVersion": "ricoberger.de/v1alpha1",
+        "kind": "VaultSecret",
+        "metadata": {"name": secret["metadata"]["name"]},
+        # Explicitly puts the key names in the object, so you can tell if a secret changed from `git diff`.
+        "spec": {"keys": [key for key in pairs], "path": f"pwned/{path}", "type": "Opaque"},
+    }
