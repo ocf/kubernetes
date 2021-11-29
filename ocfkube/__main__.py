7a671b763efe733c2b7c94327455892ef90824e0
Nikhil Jha <hi@nikhiljha.com>
2021-11-29 15:30:34 -0800

    :sparkles: use click to make cli nicer

--- ocfkube/__main__.py
+++ ocfkube/__main__.py
@@ -1,12 +1,31 @@
 import sys
 
 import ocfkube
+import click
+
+
+@click.group()
+def cli(debug):
+    pass
+
+
+@cli.command()
+@click.option("--module", required=True, type=str)
+def print(module):
+    """Generate new manifests and print them to stdout"""
+    click.echo(ocfkube.build(module, write=False))
+
+
+@cli.command()
+@click.option("-m", "--module", "module", required=False, type=str)
+def write(module: str = None):
+    """Write new manifests to the manifests directory"""
+    if module != None:
+        ocfkube.build(module, write=True)
+    else:
+        ocfkube.build_changed()
+    click.echo("Wrote manifests!")
+
 
 if __name__ == "__main__":
-    # TODO: Make this more robust...
-    if len(sys.argv) == 1:
-        ocfkube.build_changed()
-    elif len(sys.argv) == 2:
-        print(ocfkube.build(sys.argv[1]))
-    else:
-        raise ValueError(f"usage: {sys.argv[0]} <application name>")
+    cli()
