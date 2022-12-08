c8a968da25bf34b9430ebd5799ee8b0839c0d533
Oliver Ni <oliver.ni@gmail.com>
2022-12-07 21:32:44 -0800

    Move argoci to helm

---
+++ apps/argo-workflows.py
@@ -0,0 +1,16 @@
+from typing import Generator
+
+from transpire import helm
+
+from apps.versions import versions
+
+name = "argoci"
+
+
+def objects() -> Generator[dict, None, None]:
+    # TODO: Create argo-events namespace.
+
+    yield from helm.build_chart_from_versions(
+        name="argo-workflows",
+        versions=versions,
+    )
