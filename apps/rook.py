2459e9a53604ba1d8a144b080525665a44484496
Nikhil Jha <nikhiljha@users.noreply.github.com>
2022-12-28 11:04:06 -0800

    :sparkles: deploy OCF Object Ocean (O3)
    OCF Object Ocean is an HDD-backed S3-compatible object storage system
    with metadata stored on NVMe drives for better latency on meta
    operations. The plan is to use this for all object storage needs, and to
    take advantage of multitenancy to eventually offer this as a service to
    the broader campus community.

--- apps/rook.py
+++ apps/rook.py
@@ -2,6 +2,7 @@ from transpire import helm
 
 from apps.versions import versions
 
+name = "rook"
 values = {
     "image": {
         "tag": "v1.10.6",
@@ -14,6 +15,7 @@ values = {
     "dashboard": {
         "enabled": True,
     },
+    "enableDiscoveryDaemon": True,
     # <https://rook.io/docs/rook/v1.10/Getting-Started/Prerequisites/prerequisites/?h=nixos#nixos>
     # <https://github.com/rook/rook/blob/v1.10.6/deploy/charts/rook-ceph/values.yaml#L141>
     "csi": {
@@ -40,80 +42,96 @@ values = {
     },
 }
 
-name = "rook"
-
-nucleusDevices = (
-    [
-        {
-            "config": {"deviceClass": "nvme"},
-            "name": "/dev/disk/by-path/pci-0000:02:00.0-nvme-1",
-        },
-        {
-            "config": {"deviceClass": "nvme"},
-            "name": "/dev/disk/by-path/pci-0000:41:00.0-nvme-1",
-        },
-    ],
-)
-
-ceph_cluster = {
-    "apiVersion": "ceph.rook.io/v1",
-    "kind": "CephCluster",
-    "metadata": {
-        "name": "ceph",
-        "namespace": "rook",
-        "annotations": {"argocd.argoproj.io/compare-options": "IgnoreExtraneous"},
-    },
-    "spec": {
-        "cephVersion": {"image": "quay.io/ceph/ceph:v17.2.5"},
-        "dataDirHostPath": "/var/lib/rook",
-        "mon": {"count": 3, "allowMultiplePerNode": False},
-        "dashboard": {
-            "enabled": True,
-        },
-        "storage": {
-            "nodes": [
-                {
-                    "devices": nucleusDevices,
-                    "name": "adenine",
-                },
-                {
-                    "devices": nucleusDevices,
-                    "name": "guanine",
-                },
-                {
-                    "devices": nucleusDevices,
-                    "name": "cytosine",
-                },
-                {"name": "thymine"},
-            ],
-            "useAllDevices": False,
-            "useAllNodes": False,
-        },
-    },
-}
-
-storageclass_yaml = [
+nucleusDevices = [
     {
+        "config": {"deviceClass": "nvme"},
+        "name": "/dev/disk/by-path/pci-0000:02:00.0-nvme-1",
+    },
+    {
+        "config": {"deviceClass": "nvme"},
+        "name": "/dev/disk/by-path/pci-0000:41:00.0-nvme-1",
+    },
+    {
+        "config": {"deviceClass": "hdd"},
+        "name": "/dev/disk/by-path/pci-0000:84:00.0-ata-2",
+    },
+    {
+        "config": {"deviceClass": "hdd"},
+        "name": "/dev/disk/by-path/pci-0000:84:00.0-ata-3",
+    },
+]
+
+
+def objects():
+    yield from helm.build_chart_from_versions(
+        name="rook",
+        versions=versions,
+        values=values,
+    )
+
+    yield {
+        "apiVersion": "ceph.rook.io/v1",
+        "kind": "CephCluster",
+        "metadata": {
+            "name": "ceph",
+            "namespace": "rook",
+            "annotations": {"argocd.argoproj.io/compare-options": "IgnoreExtraneous"},
+        },
+        "spec": {
+            "cephVersion": {"image": "quay.io/ceph/ceph:v17.2.5"},
+            "dataDirHostPath": "/var/lib/rook",
+            "mon": {"count": 3, "allowMultiplePerNode": False},
+            # Re-enable this when the issue that blocks it is merged and available in a released Ceph version.
+            # <https://github.com/ceph/ceph/pull/48384>
+            # This is needed to show physical devices in the Ceph dashboard.
+            # "mgr": {'modules': [{'name': 'rook', 'enabled': True}]},
+            "dashboard": {
+                "enabled": True,
+            },
+            "storage": {
+                "nodes": [
+                    {
+                        "devices": nucleusDevices,
+                        "name": "adenine",
+                    },
+                    {
+                        "devices": nucleusDevices,
+                        "name": "guanine",
+                    },
+                    {
+                        "devices": nucleusDevices,
+                        "name": "cytosine",
+                    },
+                    {"name": "thymine"},
+                ],
+                "useAllDevices": False,
+                "useAllNodes": False,
+            },
+        },
+    }
+
+    yield {
         "apiVersion": "ceph.rook.io/v1",
         "kind": "CephBlockPool",
-        "metadata": {"name": "replicapool", "namespace": "rook"},
+        "metadata": {"name": "rbd-nvme", "namespace": "rook"},
         "spec": {
             "failureDomain": "host",
-            "replicated": {"size": 2, "requireSafeReplicaSize": True},
-            "deviceClass": "ssd",
+            "replicated": {"size": 3, "requireSafeReplicaSize": True},
+            "deviceClass": "nvme",
         },
-    },
-    {
+    }
+
+    yield {
         "apiVersion": "storage.k8s.io/v1",
         "kind": "StorageClass",
         "metadata": {
-            "name": "rook-ceph-block",
+            "name": "rbd-nvme",
             "annotations": {"storageclass.kubernetes.io/is-default-class": "true"},
         },
         "provisioner": "rook.rbd.csi.ceph.com",
         "parameters": {
             "clusterID": "rook",
-            "pool": "replicapool",
+            "pool": "rbd-nvme",
             "imageFormat": "2",
             "csi.storage.k8s.io/provisioner-secret-name": "rook-csi-rbd-provisioner",
             "csi.storage.k8s.io/provisioner-secret-namespace": "rook",
@@ -127,16 +145,79 @@ storageclass_yaml = [
         },
         "allowVolumeExpansion": True,
         "reclaimPolicy": "Delete",
-    },
-]
+    }
 
+    crtname = "o3-ocf-crt"
+    yield {
+        "apiVersion": "cert-manager.io/v1",
+        "kind": "Certificate",
+        "metadata": {"name": crtname},
+        "spec": {
+            "secretName": crtname,
+            "dnsNames": ["o3.ocf.io", "o3.ocf.berkeley.edu"],
+            "issuerRef": {
+                "name": "letsencrypt",
+                "kind": "ClusterIssuer",
+                "group": "cert-manager.io",
+            },
+        },
+    }
 
-def objects():
-    yield from helm.build_chart_from_versions(
-        name="rook",
-        versions=versions,
-        values=values,
-    )
+    yield {
+        "apiVersion": "networking.k8s.io/v1",
+        "kind": "Ingress",
+        "metadata": {
+            "name": "o3-ingress",
+        },
+        "spec": {
+            "ingressClassName": "cilium",
+            "tls": [
+                {
+                    "hosts": ["o3.ocf.io", "o3.ocf.berkeley.edu"],
+                    "secretName": crtname,
+                },
+            ],
+            "rules": [
+                {
+                    "host": "o3.ocf.io",
+                    "http": {
+                        "paths": [
+                            {
+                                "path": "/",
+                                "pathType": "Prefix",
+                                "backend": {
+                                    "service": {"name": "rook-ceph-rgw-rgw-hdd", "port": {"number": 80}}
+                                },
+                            }
+                        ]
+                    }
+                }
+            ],
+        },
+    }
 
-    yield ceph_cluster
-    yield from storageclass_yaml
+    yield {
+        "apiVersion": "ceph.rook.io/v1",
+        "kind": "CephObjectStore",
+        "metadata": {"name": "rgw-hdd"},
+        "spec": {
+            "metadataPool": {
+                "failureDomain": "host",
+                "replicated": {"size": 3},
+                "deviceClass": "nvme",
+            },
+            "dataPool": {
+                "failureDomain": "host",
+                "replicated": {"size": 3},
+                "deviceClass": "hdd",
+            },
+            "preservePoolsOnDelete": True,
+            "gateway": {
+                "securePort": 443,
+                "port": 80,
+                "sslCertificateRef": crtname,
+                "instances": 1,
+            },
+            "healthCheck": {"bucket": {"disabled": False, "interval": "120s"}},
+        },
+    }
