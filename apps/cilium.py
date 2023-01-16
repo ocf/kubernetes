fc01f86fcf2c786bf96ead8c9ea91b2a6a09083c
Ethan Wu <ethanwu10@gmail.com>
2023-01-15 22:41:55 -0800

    fix: revert to cilium stable

--- apps/cilium.py
+++ apps/cilium.py
@@ -38,34 +38,5 @@ def objects():
                 "requireIPv6PodCIDR": True,
                 "requireIPv4PodCIDR": True,
             },
-            "ingressController": {
-                "enabled": True,
-                "loadbalancerMode": "shared",
-                "enforceHttps": True,
-                "service": {
-                    # Hardcode the shared loadbalancer IP, since lots of external DNS points to it and it should not change easily.
-                    "annotations": {
-                        "metallb.universe.tf/loadBalancerIPs": "169.229.226.81,2607:f140:8801::1:81"
-                    },
-                },
-                # first two are default values, we aren't using them as of 2022
-                "ingressLBAnnotationPrefixes": [
-                    "service.beta.kubernetes.io",
-                    "service.kubernetes.io",
-                    "metallb.universe.tf",
-                ],
-            },
-            # <https://github.com/cilium/cilium/issues/22949>
-            "image": {
-                "repository": "quay.io/cilium/cilium-ci",
-                "tag": "0d91314f403cb906426bd6c13a1e5e0839be7df8",
-                "digest": "sha256:f217be397d00865f0c61964ea9be944294b9e8baaf0126185ff940b1433bb510",
-            },
-            # <https://github.com/cilium/cilium/issues/22668>
-            "operator": {
-                "image": {
-                    "override": "quay.io/cilium/operator-generic-ci@sha256:a26bcd90fb3b665009aedb551d6efa14e543290441f7402444cad10595ae4a45",
-                },
-            },
         },
     )
