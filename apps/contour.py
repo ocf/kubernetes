7ff21faeebc17020f398815f850c8945c30e1580
Nikhil Jha <nikhiljha@users.noreply.github.com>
2023-01-15 22:32:58 -0800

    feat: switch to contour as ingress controller
    We need cilium -rc to have any of the ingress controller stuff working,
    but this also has so many bugs that the cluster itself is just all
    around unstable and broken. I think this is a sign that we should not be
    using cilium ingress controller.

---
+++ apps/contour.py
@@ -0,0 +1,21 @@
+from transpire import helm
+from transpire.utils import get_versions
+
+name = "contour"
+
+
+def objects():
+    yield from helm.build_chart_from_versions(
+        name=name,
+        versions=get_versions(__file__),
+        values={
+            "envoy": {
+                "service": {
+                    "annotations": {
+                        "metallb.universe.tf/loadBalancerIPs": "169.229.226.81,2607:f140:8801::1:81",
+                    },
+                    "ipFamilyPolicy": "PreferDualStack",
+                },
+            },
+        },
+    )
