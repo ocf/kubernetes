987602a4d5bf1e1751494553dfd0b2e1fbd22f8c
Nikhil Jha <nikhiljha@users.noreply.github.com>
2023-01-29 14:22:45 -0800

    feat: kubevirt for VMs
    This should check a checksum in the future via a helper in transpire.

---
+++ apps/kubevirt.py
@@ -0,0 +1,30 @@
+from typing import Generator
+
+import requests
+import yaml
+from transpire.utils import get_versions
+
+name = "kubevirt"
+
+
+def objects() -> Generator[dict, None, None]:
+    version = get_versions(__file__)[name]["version"]
+    print(version)
+    yield from yaml.safe_load_all(
+        requests.get(
+            f"https://github.com/kubevirt/kubevirt/releases/download/{version}/kubevirt-operator.yaml"
+        ).text
+    )
+
+    yield {
+        "apiVersion": "kubevirt.io/v1",
+        "kind": "KubeVirt",
+        "metadata": {"name": "kubevirt", "namespace": "kubevirt"},
+        "spec": {
+            "certificateRotateStrategy": {},
+            "configuration": {"developerConfiguration": {"featureGates": []}},
+            "customizeComponents": {},
+            "imagePullPolicy": "IfNotPresent",
+            "workloadUpdateStrategy": {},
+        },
+    }
