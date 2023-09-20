3c22d7a783abbd3b9417f565ae9c3a2130821a78
Oliver Ni <oliver.ni@gmail.com>
2023-09-19 17:08:18 -0700

    feat: add gvisor

---
+++ apps/gvisor.py
@@ -0,0 +1,21 @@
+name = "gvisor"
+
+
+def objects():
+    yield {
+        "apiVersion": "node.k8s.io/v1",
+        "kind": "RuntimeClass",
+        "metadata": {"name": "gvisor"},
+        "handler": "runsc",
+    }
+
+    # Test pod
+    yield {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {"name": "nginx-gvisor"},
+        "spec": {
+            "runtimeClassName": "gvisor",
+            "containers": [{"name": "nginx", "image": "nginx"}],
+        },
+    }
