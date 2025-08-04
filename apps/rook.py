40c3f9477494ac21de6c69becd697ffb8025a7a4
sophiebsw <sophiebsw@gmail.com>
2025-08-04 02:09:35 -0700

    update ceph version to 19.2.3

--- apps/rook.py
+++ apps/rook.py
@@ -77,7 +77,7 @@ def objects():
             "annotations": {"argocd.argoproj.io/compare-options": "IgnoreExtraneous"},
         },
         "spec": {
-            "cephVersion": {"image": "quay.io/ceph/ceph:v18.2.2"},
+            "cephVersion": {"image": "quay.io/ceph/ceph:v19.2.3"},
             "dataDirHostPath": "/var/lib/rook",
             "mon": {"count": 3, "allowMultiplePerNode": False},
             # Re-enable this when the issue that blocks it is merged and available in a released Ceph version.
