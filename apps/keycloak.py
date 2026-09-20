c191e866ffabc71ffb2aeb5507287c5f40a25a5f
sophiebsw <sophiebsw@gmail.com>
2026-09-20 15:03:41 -0700

    keycloak: add kubernetes-prod client

--- apps/keycloak.py
+++ apps/keycloak.py
@@ -64,6 +64,15 @@ clients = [
         },
     ),
     oidc_client(
+        "kubernetes-prod",
+        name="kubernetes-prod",
+        description="OCF Kubernetes Production Cluster",
+        redirect_uris=[
+            "http://localhost:8000",
+            "http://localhost:18000"
+        ],
+    ),
+    oidc_client(
         "kubernetes-staging",
         name="kubernetes-staging",
         description="OCF Kubernetes Staging Cluster",
